b665292fb31b2f868955baf326eab1fc071ff96c
Mae Thakur <35869034+pinkcustard2@users.noreply.github.com>
2026-01-22 16:47:23 +0000

    i made it pink

---
+++ src/Main.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include <gl/gl.h>
+#include <GLFW/glfw3.h>
+using namespace std;
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Platformer", NULL, NULL);
+    if (window == NULL)
+    {
+        cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glViewport(0, 0, 800, 600);
+
+    glfwMakeContextCurrent(window);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+        glClearColor(1.0f, 0.3f, 0.7f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
